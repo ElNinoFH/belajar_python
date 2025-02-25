749987059f972ee2e0d157b413aeffc758b484d9
ElNinoFH <elninofhmz@gmail.com>
2025-02-25 20:18:25 +0700

    range and slicing

---
+++ range.py
@@ -0,0 +1,20 @@
+numbers = range(11)
+# secara default 0 adalah start
+print(numbers)
+print(list(numbers))
+
+numbers = range(50, 101)
+print(numbers)
+print(list(numbers))
+
+# numbers = range(10,31,5)
+# print(numbers)
+# print(list(numbers))
+
+# range(batas awal, batas akhir, selisih)
+
+print(numbers[1])
+print(numbers[5])
+
+print(numbers[-4])
+print(numbers[-1])
---
+++ soalrange.py
@@ -0,0 +1,17 @@
+# create a variable called 'numbers'
+# creat a list of numbers 1 - 250 using list(range())
+numbers = list(range(1,251))
+
+# print a number from index 15
+print(numbers[15])
+
+# print an eight number from the back
+# clue : negative indexing
+print(numbers[-8])
+
+# print a group of numbers, start from 55 until 67
+print(numbers[55:68])
+
+# print a group of even numbers, starts from 70 until 240
+# clue : use step parameter
+print(numbers[70:241:2])
